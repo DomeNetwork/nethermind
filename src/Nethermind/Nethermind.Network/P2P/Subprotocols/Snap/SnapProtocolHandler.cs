ce0ebdb1cd4b4223e17265bcd0941e966f0a5493
DCeleda <dceleda@hotmail.com>
2021-11-16 12:42:25 +0000

    SNAP messages

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/SnapProtocolHandler.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/SnapProtocolHandler.cs
@@ -62,14 +62,7 @@ namespace Nethermind.Network.P2P.Subprotocols.Snap
             add { }
             remove { }
         }
-        
-        public override string Name => "snap1";
-        protected override TimeSpan InitTimeout => Timeouts.Eth;
 
-        public override byte ProtocolVersion => 1;
-        public override string ProtocolCode => Protocol.Snap;
-        public override int MessageIdSpaceSize => 8;
-        
         public override void Init()
         {
             ProtocolInitialized?.Invoke(this, new ProtocolInitializedEventArgs(this));
@@ -81,108 +74,7 @@ namespace Nethermind.Network.P2P.Subprotocols.Snap
         
         public override void HandleMessage(ZeroPacket message)
         {
-            //TODO: add unit test
-            if (!CheckIfEthStatusMsgReceived())
-            {
-                throw new SubprotocolException(
-                    $"No {nameof(StatusMessage)} received prior to communication with {Session?.Node:c}.");
-            }
             
-            int packetType = message.PacketType;
-            if (Logger.IsTrace)
-                Logger.Trace(
-                    $"{Counter:D5} {Eth62MessageCode.GetDescription(packetType)} from {Session?.Node:c}");
-
-            switch (packetType)
-            {
-                case SnapMessageCode.GetAccountRange:
-                    GetAccountRangeMessage getAccRangeMsg = Deserialize<GetAccountRangeMessage>(message.Content);
-                    ReportIn(getAccRangeMsg);
-                    Handle(getAccRangeMsg);
-                    break;
-                case SnapMessageCode.AccountRange:
-                    AccountRangeMessage accRangeMsg = Deserialize<AccountRangeMessage>(message.Content);
-                    ReportIn(accRangeMsg);
-                    Handle(accRangeMsg);
-                    break;
-                case SnapMessageCode.GetStorageRanges:
-                    GetStorageRangesMessage getStorageRangesMsg = Deserialize<GetStorageRangesMessage>(message.Content);
-                    ReportIn(getStorageRangesMsg);
-                    Handle(getStorageRangesMsg);
-                    break;
-                case SnapMessageCode.StorageRanges:
-                    StorageRangesMessage storageRangesMsg = Deserialize<StorageRangesMessage>(message.Content);
-                    ReportIn(storageRangesMsg);
-                    Handle(storageRangesMsg);
-                    break;
-                case SnapMessageCode.GetByteCodes:
-                    GetByteCodesMessage getByteCodesMsg = Deserialize<GetByteCodesMessage>(message.Content);
-                    ReportIn(getByteCodesMsg);
-                    Handle(getByteCodesMsg);
-                    break;
-                case SnapMessageCode.ByteCodes:
-                    ByteCodesMessage byteCodesMsg = Deserialize<ByteCodesMessage>(message.Content);
-                    ReportIn(byteCodesMsg);
-                    Handle(byteCodesMsg);
-                    break;
-                case SnapMessageCode.GetTrieNodes:
-                    GetTrieNodesMessage getTrieNodesMsg = Deserialize<GetTrieNodesMessage>(message.Content);
-                    ReportIn(getTrieNodesMsg);
-                    Handle(getTrieNodesMsg);
-                    break;
-                case SnapMessageCode.TrieNodes:
-                    TrieNodesMessage trieNodesMsg = Deserialize<TrieNodesMessage>(message.Content);
-                    ReportIn(trieNodesMsg);
-                    Handle(trieNodesMsg);
-                    break;
-            }
-        }
-
-        private void Handle(GetAccountRangeMessage msg)
-        {
-            throw new NotImplementedException();
-        }
-        private void Handle(AccountRangeMessage msg)
-        {
-            throw new NotImplementedException();
-        }
-        private void Handle(GetStorageRangesMessage msg)
-        {
-            throw new NotImplementedException();
-        }private void Handle(StorageRangesMessage msg)
-        {
-            throw new NotImplementedException();
-        }private void Handle(GetByteCodesMessage msg)
-        {
-            throw new NotImplementedException();
-        }private void Handle(ByteCodesMessage msg)
-        {
-            throw new NotImplementedException();
-        }private void Handle(GetTrieNodesMessage msg)
-        {
-            throw new NotImplementedException();
-        }private void Handle(TrieNodesMessage msg)
-        {
-            throw new NotImplementedException();
-        }
-            
-        private bool CheckIfEthStatusMsgReceived()
-        {
-            if (_ethStatusReceived)
-            {
-                return true;
-            }
-            
-            if (Session.TryGetProtocolHandler(Protocol.Eth, out IProtocolHandler handler))
-            {
-                if (handler is Eth62ProtocolHandler { StatusReceived: true })
-                {
-                    _ethStatusReceived = true;
-                    return true;
-                }
-            }
-
-            return false;
         }
 
         public override void DisconnectProtocol(DisconnectReason disconnectReason, string details)
